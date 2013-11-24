d6d980dbaf3c813aac5dc60cfe2d378888590c1a
marcusmyers <marcusmyers@gmail.com>
2013-11-24 17:53:26 -0500

    removed alert after done

--- login.php
+++ login.php
@@ -30,6 +30,7 @@
       <?php
       if(isset($_SESSION['error'])){
         echo '<div class="alert alert-danger">'.$_SESSION['error'].'</div>';
+        unset($_SESSION['error']);
       }
       ?>
 

b98613c69f176801cb908a99231f29a0bb2dfbe3
marcusmyers <marcusmyers@gmail.com>
2013-11-24 16:50:20 -0500

    Added login.php file

--- login.php
+++ login.php
@@ -1,22 +1,20 @@
-<?php session_start(); 
-if(!isset($_SESSION['user'])){
-  header('Location login.php');
-}
-?>
+<?php session_start(); ?>
 <!DOCTYPE html>
 <html lang="en">
   <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="">
-    <meta name="author" content="">
+    <meta name="description" content="Home Voicemail Server">
+    <meta name="author" content="Mark Myers">
 
-    <title>Home </title>
+    <title>Signin </title>
 
     <!-- Bootstrap core CSS -->
     <link href="/css/bootstrap.css" rel="stylesheet">
 
+    <!-- Custom styles for this template -->
+    <link href="/css/signin.css" rel="stylesheet">
 
     <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
@@ -29,6 +27,15 @@ if(!isset($_SESSION['user'])){
 
     <div class="container">
 
+      <form class="form-signin">
+        <h2 class="form-signin-heading">Please sign in</h2>
+        <input type="text" class="form-control" placeholder="Email address" required autofocus>
+        <input type="password" class="form-control" placeholder="Password" required>
+        <label class="checkbox">
+          <input type="checkbox" value="remember-me"> Remember me
+        </label>
+        <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+      </form>
 
     </div> <!-- /container -->
 
@@ -36,8 +43,6 @@ if(!isset($_SESSION['user'])){
     <!-- Bootstrap core JavaScript
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
-    <script src='http://code.jquery.com/jquery-1.10.2.min.js'></script>
-    <script src='/js/bootstrap.min.js'></script>
   </body>
 </html>
 

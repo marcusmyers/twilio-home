81f3306b2471bc167b2e93454737be99087db205
marcusmyers <marcusmyers@gmail.com>
2013-11-25 11:28:45 -0500

    Fixed time to be less than 17

--- voice/handle-key.php
+++ voice/handle-key.php
@@ -11,14 +11,14 @@ echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
 ?>
 <Response>
 <?php if ($_REQUEST['Digits'] == '1') { 
-   if ($time <= 17 || $time >= 22){ ?>
+   if ($time < 17 || $time >= 22){ ?>
   <Redirect method="GET">http://openvbx.emoxter.com/voicemail/leave_a_message.php?exten=1001</Redirect>
   <?php } else { ?>
   <Dial>+14197222855</Dial>
   <Say>The call failed or the remote party hung up. Goodbye.</Say>
 <?php  } 
  } elseif ($_REQUEST['Digits'] == '2') {
-  if ($time <= 17 || $time >= 22){ ?>
+  if ($time < 17 || $time >= 22){ ?>
   <Redirect method="GET">http://openvbx.emoxter.com/voicemail/leave_a_message.php?exten=1000</Redirect>
   <?php } else { ?>
   <Dial>+14199574216</Dial>

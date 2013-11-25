42e5ccf409138160b6b925f715366a11cadf4e66
marcusmyers <marcusmyers@gmail.com>
2013-11-24 19:16:45 -0500

    Added time limits to the voicemail

--- voice/handle-key.php
+++ voice/handle-key.php
@@ -10,11 +10,19 @@ echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
 ?>
 <Response>
 <?php if ($_REQUEST['Digits'] == '1') { ?>
+  <?php if (date('G') < 17 || date('G') > 22){ ?>
+  <Redirect method="GET">http://openvbx.emoxter.com/voicemail/leave_a_message.php?exten=1001</Redirect>
+  <?php } else { ?>
   <Dial>+14197222855</Dial>
   <Say>The call failed or the remote party hung up. Goodbye.</Say>
+  <?php  } ?>
 <?php } elseif ($_REQUEST['Digits'] == '2') { ?>
+  <?php if (date('G') < 17 || date('G') > 22){ ?>
+  <Redirect method="GET">http://openvbx.emoxter.com/voicemail/leave_a_message.php?exten=1001</Redirect>
+  <?php } else { ?>
   <Dial>+14199574216</Dial>
   <Say>The call failed or the remote party hung up. Goodbye.</Say>
+  <?php  } ?>
 <?php } elseif ($_REQUEST['Digits'] == '3') { ?>
   <Redirect method="GET">http://openvbx.emoxter.com/voicemail/leave_a_message.php?exten=1234</Redirect>
 <?php } ?>
